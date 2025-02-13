65e9ac2912cf8b2a5e46e3de00a703347d4beb93
Christian Iza <ciizao@uce.edu.ec>
2025-02-13 16:58:39 -0500

    feat: :sparkles: Ad cors create product

--- products-management/create-product-service/src/app.js
+++ products-management/create-product-service/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const sequelize = require("./config/database");
 const productRoutes = require("./router/routes"); 
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
