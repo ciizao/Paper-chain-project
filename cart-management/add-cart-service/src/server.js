3a565f6c939933943f9a5d6c6ed871778b828973
Christian Iza <ciizao@uce.edu.ec>
2025-02-12 21:54:45 -0500

    feat: :bug: Modify cors

--- cart-management/add-cart-service/src/server.js
+++ cart-management/add-cart-service/src/server.js
@@ -5,7 +5,12 @@ const connectDB = require("./config/database");
 const cartRoutes = require("./routes/cartRoutes");
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 
 app.use(express.json());
 
